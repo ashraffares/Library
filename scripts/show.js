a999ccbcd6a855dc5c293dac51dfc623499cdef8
Fares <ashraffares090@gmail.com>
2021-05-21 09:31:57 +0200

    Fix unused function error

--- scripts/show.js
+++ scripts/show.js
@@ -1,24 +1,24 @@
 const list = JSON.parse(localStorage.getItem('data'));
-function destroyBook(index) {
+function destroy_book(index) {
   list.splice(index, 1);
-  localStorage.setItem('data', JSON.stringify(list));
+  localStorage.setItem("data", JSON.stringify(list));
   document.location.reload();
 }
 
 function status(index) {
-  if (list[index].read === 'Not read yet') {
+  if (list[index].read == 'Not read yet') {
     list[index].read = 'read it';
   } else {
     list[index].read = 'Not read yet';
   }
-  localStorage.setItem('data', JSON.stringify(list));
+  localStorage.setItem("data", JSON.stringify(list));
   document.location.reload();
 }
 
-const len = list.length;
-for (let i = 0; i < len; i += i) {
+let len = list.length;
+for (let i = 0; i < len; i++) {
   const tr = document.createElement('tr');
-  const addBook = document.getElementById('table_body');
+  const add_book = document.getElementById("table_body");
   const td = `
     <td>${[i]}</td>
     <td>${list[i].title}</td>
@@ -26,8 +26,8 @@ for (let i = 0; i < len; i += i) {
     <td>${list[i].pages}</td>
     <td>${list[i].read}</td>
     <td><button onclick="status(${[i]})" class="btn btn-sm btn-success">status</button></td>
-    <td><button onclick="destroyBook(${[i]})" class="btn btn-sm btn-danger">delete</button></td>`;
+    <td><button onclick="destroy_book(${[i]})" class="btn btn-sm btn-danger">delete</button></td>`;
 
   tr.innerHTML = td;
-  addBook.appendChild(tr);
+  add_book.appendChild(tr);
 }
