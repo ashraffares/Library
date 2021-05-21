19a5d3347be9e7a69c04dd4375121e932b6ec4b0
Fares <ashraffares090@gmail.com>
2021-05-21 08:08:10 +0200

    Fix indentation error with prettier

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,22 @@
-if (localStorage.getItem('data') == null) {
-    localStorage.setItem('data', '[]');
+if (localStorage.getItem("data") == null) {
+  localStorage.setItem("data", "[]");
 }
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
-let button = document.getElementById('submitform').onclick = () => {
-    const bk = new Book(
-        document.getElementById('title').value,
-        document.getElementById('author').value,
-        document.getElementById('pages').value,
-        document.getElementById('read').value
-    )
-    var add = JSON.parse(localStorage.getItem('data'));
-    add.push(bk);
-    localStorage.setItem('data', JSON.stringify(add));
-    window.open('./show.html', '_self')
-}
+let button = (document.getElementById("submitform").onclick = () => {
+  const bk = new Book(
+    document.getElementById("title").value,
+    document.getElementById("author").value,
+    document.getElementById("pages").value,
+    document.getElementById("read").value
+  );
+  var add = JSON.parse(localStorage.getItem("data"));
+  add.push(bk);
+  localStorage.setItem("data", JSON.stringify(add));
+  window.open("./show.html", "_self");
+});
--- scripts/show.js
+++ scripts/show.js
@@ -1,37 +1,39 @@
-list = JSON.parse(localStorage.getItem('data'));
+list = JSON.parse(localStorage.getItem("data"));
 function destroy_book(index) {
-    list.splice(index, 1);
-    localStorage.setItem('data', JSON.stringify(list))
-    document.location.reload()
+  list.splice(index, 1);
+  localStorage.setItem("data", JSON.stringify(list));
+  document.location.reload();
 }
 
 function status(index) {
-
-    if (list[index].read == 'Not read yet') {
-        list[index].read = 'read it'
-    } else {
-        list[index].read = 'Not read yet';
-    }
-    localStorage.setItem('data', JSON.stringify(list))
-    document.location.reload()
+  if (list[index].read == "Not read yet") {
+    list[index].read = "read it";
+  } else {
+    list[index].read = "Not read yet";
+  }
+  localStorage.setItem("data", JSON.stringify(list));
+  document.location.reload();
 }
-
 
 let len = list.length;
 for (let i = 0; i < len; i++) {
-    let tr = document.createElement('tr')
-    let add_book = document.getElementById('table_body');
-    const td = `
+  let tr = document.createElement("tr");
+  let add_book = document.getElementById("table_body");
+  const td = `
     <td>${[i]}</td>
     <td>${list[i].title}</td>
     <td>${list[i].author}</td>
     <td>${list[i].pages}</td>
     <td )">${list[i].read}</td>
-    <td><button onclick="status(${[i]})" class="btn btn-sm btn-success">status</button></td>
-    <td><button onclick="destroy_book(${[i]})" class="btn btn-sm btn-danger">delete</button></td>
+    <td><button onclick="status(${[
+      i,
+    ]})" class="btn btn-sm btn-success">status</button></td>
+    <td><button onclick="destroy_book(${[
+      i,
+    ]})" class="btn btn-sm btn-danger">delete</button></td>
     
     `;
 
-    tr.innerHTML = td;
-    add_book.appendChild(tr)
+  tr.innerHTML = td;
+  add_book.appendChild(tr);
 }
